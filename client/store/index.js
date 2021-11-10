3d9217afab8ab980a044a001c32225b2dc766415
Kelsey <pages.kelsey@gmail.com>
2021-11-10 15:10:46 -0500

    completed redux/component for SingleProduct render only

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,13 @@ import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import auth from "./auth";
 import productsReducer from "../redux/products";
+import singleProductReducer from "../redux/singleProduct";
 
-const reducer = combineReducers({ auth, products: productsReducer });
+const reducer = combineReducers({
+  auth,
+  products: productsReducer,
+  product: singleProductReducer,
+});
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
