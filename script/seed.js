81d1e94bf726d48d719d55de3017574ecf8ffbbe
hector nevarez <hanevarezg@gmail.com>
2021-11-11 13:50:59 -0800

    Seeding successfully Users, Products, Orders & Carts. Not sure if associations will work

--- script/seed.js
+++ script/seed.js
@@ -2,7 +2,7 @@
 
 const {
   db,
-  models: { User, Product },
+  models: { User, Product, Order, Cart },
 } = require("../server/db");
 const faker = require("faker");
 
@@ -10,88 +10,111 @@ const faker = require("faker");
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
-// function randomCategory(max) {
-//   return Math.floor(Math.random() * max) + 1;
-// }
-// const values = ["chair", "sofa", "table", "dresser"];
-// let randomCate= values[randomCategory(3)];
+function randomNumber(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+const values = ["chair", "sofa", "table", "dresser"];
+let randomCategory = values[randomNumber(3)];
 
-// // Products Dummy data
-// const dummyProducts = [
-//   {
-//     name: faker.commerce.productName(),
-//     description: faker.commerce.productDescription(),
-//     imageUrl: faker.image.imageUrl(),
-//     price: faker.commerce.price(),
-//     inventory: faker.datatype.number(),
-//     category: randomCate,
-//   }
-// ];
+function generateUsers(num) {
+  let users = []
+  for (let i = 0; i < num; i++) {
+    users.push({
+      email: faker.internet.email(),
+      password: '123',
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      shippingAddress: `${faker.address.streetAddress()},${faker.address.city()},${faker.address.state()},${faker.address.zipCode()}`,
+      billingAddress: `${faker.address.streetAddress()},${faker.address.city()},${faker.address.state()},${faker.address.zipCode()}`,
+      isAdmin: faker.datatype.boolean(),
+      // creditCard: faker.finance.creditCardNumber(),
+    });
+  }
+  return users;
+};
 
-const dummyProducts = [
-  {
-    name: "peachyChair",
-    imageUrl:
-      "https://media.istockphoto.com/photos/liivng-coralcolor-of-the-year-2019interior-design-for-living-area-or-picture-id1134702834?b=1&k=6&m=1134702834&s=170667a&w=0&h=2q1rjh0eKl02t3ZCfbeweubkljyd64fZJON5862nXRg=",
-    description: "Its a cute lil sofa!",
-    price: 800,
-    inventory: 100,
-    category: "chair",
-  },
-  {
-    name: "peachySofa",
-    imageUrl:
-      "https://media.istockphoto.com/photos/liivng-coralcolor-of-the-year-2019interior-design-for-living-area-or-picture-id1134702834?b=1&k=6&m=1134702834&s=170667a&w=0&h=2q1rjh0eKl02t3ZCfbeweubkljyd64fZJON5862nXRg=",
-    description: "Its a cute lil sofa!",
-    price: 2200,
-    inventory: 5,
-    category: "sofa",
-  },
-  {
-    name: "peachyTable",
-    imageUrl:
-      "   https://images.urbndata.com/is/image/Anthropologie/60451051_012_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=960",
-    description: "Its a cute lil table!",
-    price: 1300,
-    inventory: 11,
-    category: "table",
-  },
-  {
-    name: "peachyDresser",
-    imageUrl:
-      "https://images.urbndata.com/is/image/Anthropologie/40800781_006_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
-    description: "Its a cute lil dresser!",
-    price: 2400,
-    inventory: 22,
-    category: "dresser",
-  },
-];
+function generateProducts(num) {
+  let products = []
+  for (let i = 0; i < num; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.imageUrl(),
+      price: faker.commerce.price(),
+      inventory: faker.datatype.number(),
+      category: randomCategory,
+    });
+  }
+  return products;
+};
+
+function generateOrders(num) {
+  let orders = []
+  for (let i = 1; i <= num; i++) {
+    orders.push({
+      userId: i,
+    });
+  }
+  return orders;
+};
+
+function generateCarts(num) {
+  let carts = []
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= 4; j++) {
+      carts.push({
+        orderId: i,
+        productId:j,
+      });
+    }
+  }
+  return carts;
+}
+
+// Creating the dummy data
+const qtyOfUsers = 10;
+const qtyOfProducts = 20;
+const dummyUsers = generateUsers(qtyOfUsers);
+const dummyProducts = generateProducts(qtyOfProducts);
+const dummyOrders = generateOrders(qtyOfUsers);
+const dummyCarts= generateCarts(qtyOfUsers);
 
 async function seed() {
   await db.sync({ force: true }); // clears db and matches models to tables
-  console.log("db synced!");
+  console.log(`starting seeding`);
 
   // Creating Users
-  const users = await Promise.all([
-    User.create({ email: "cody@fsa.com", password: "123" }),
-    User.create({ email: "murphy@fsa.com", password: "123" }),
-  ]);
-
+  let users = await Promise.all(
+    dummyUsers.map((user) => {
+      return User.create(user);
+    })
+  );
   // Creating Products
   let products = await Promise.all(
     dummyProducts.map((prod) => {
       return Product.create(prod);
     })
   );
+  // Creating Orders
+  let orders = await Promise.all(
+    dummyOrders.map((prod) => {
+      return Order.create(prod);
+    })
+  );
+    // Creating Carts
+  let carts = await Promise.all(
+    dummyCarts.map((prod) => {
+      return Cart.create(prod);
+    })
+  );
 
-  console.log(`seeded ${users.length} users`);
+  console.log("db synced!");
   console.log(`seeded successfully`);
   return {
-    users: {
-      cody: users[0],
-      murphy: users[1],
-    },
+    users,
     products,
+    orders,
+    carts,
   };
 }
 /*
@@ -124,5 +147,3 @@ if (module === require.main) {
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
 module.exports = seed;
-
-// https://media.istockphoto.com/photos/liivng-coralcolor-of-the-year-2019interior-design-for-living-area-or-picture-id1134702834?b=1&k=6&m=1134702834&s=170667a&w=0&h=2q1rjh0eKl02t3ZCfbeweubkljyd64fZJON5862nXRg="
