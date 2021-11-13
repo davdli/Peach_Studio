a3998263fff0a98d8f76882ab16e6a868bee2d01
Sala Yoshida <yoshidasala@gmail.com>
2021-11-13 12:26:43 -0500

    quantity

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -4,10 +4,12 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { addToCart } from "../redux/cart";
+import { useState } from "react";
 
 const SingleProduct = (props) => {
   const product = useSelector((state) => state.product);
   const user = useSelector((state) => state.auth);
+  let [quantity, changeQuantity] = useState(0);
 
   // this is like mapdispatch
   const dispatch = useDispatch();
@@ -16,11 +18,21 @@ const SingleProduct = (props) => {
     // Safe to add dispatch to the dependencies array
   }, []);
 
+  const handleQuantity = (event) => {
+    event.preventDefault();
+    console.log(quantity);
+    if (event.target.value === "increase") {
+      changeQuantity(quantity++);
+    } else if (event.target.value === "decrease") {
+      changeQuantity(quantity--);
+    }
+  };
+
   const addProduct = (event) => {
     const userObj = {
       productId: event.target.value,
       userId: user.id,
-      quantity: 3,
+      quantity: quantity,
     };
     dispatch(addToCart(userObj));
   };
@@ -33,6 +45,13 @@ const SingleProduct = (props) => {
           <h3>Price: ${product.price}</h3>
           <button className='btn first' onClick={addProduct} value={product.id}>
             Add to Cart
+          </button>
+          <button value='increase' onClick={handleQuantity}>
+            +
+          </button>
+          <div>{quantity}</div>
+          <button value='decrease' onClick={handleQuantity}>
+            -
           </button>
         </div>
         <div>
