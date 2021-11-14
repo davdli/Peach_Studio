458cc6cd4e3ca69ffd1ad6338745ad33339efb98
David Li <dvdli2323@gmail.com>
2021-11-14 10:38:04 -0500

    renders products in cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -17,9 +17,7 @@ const Cart = (props) => {
     dispatch(getCartItems(user))
   }, [])
 
-  // console.log('this is cart', cart)
-
-  return (
+  return cart.length > 0 ? (
     <div>
       <div className='cart-wrapper'>
         <h1>YOUR BAG</h1>
@@ -36,64 +34,37 @@ const Cart = (props) => {
           </Link>
         </div>
         <div className='cart-bottom'>
+
           <div className='cart-bottom-info'>
-            <div className='cart-product'>
-              <div className='cart-product-detail'>
-                <img src='https://media.istockphoto.com/photos/liivng-coralcolor-of-the-year-2019interior-design-for-living-area-or-picture-id1134702834?b=1&k=6&m=1134702834&s=170667a&w=0&h=2q1rjh0eKl02t3ZCfbeweubkljyd64fZJON5862nXRg=' />
-                <div>
-                  <p>
-                    <b>Product:</b> Peachy Chair
-                  </p>
-                  <p>
-                    <b>Id:</b> 2
-                  </p>
-                  <div
-                    className='cart-product-color'
-                    style={{ backgroundColor: "#f4d0a5" }}
-                  ></div>
+            {cart[0].products.map(product => (
+              <div className='cart-product'>
+                <div className='cart-product-detail'>
+                  <img src={product.imageUrl} />
+                  <div>
+                    <p>
+                      <b>Product:</b> {product.name}
+                    </p>
+                    <p>
+                      <b>Id:</b> {product.id}
+                    </p>
+                    <div
+                      className='cart-product-color'
+                      style={{ backgroundColor: "#f4d0a5" }}
+                    ></div>
+                  </div>
+                </div>
+                <div className='cart-product-price'>
+                  <div className='cart-product-cost'>
+                    <p>{product.price}</p>
+                  </div>
+                  <div className='cart-product-amount'>
+                    <button>+</button>
+                    <p>3</p>
+                    <button>-</button>
+                  </div>
                 </div>
               </div>
-              <div className='cart-product-price'>
-                <div className='cart-product-cost'>
-                  <p>$100</p>
-                </div>
-                <div className='cart-product-amount'>
-                  <button>+</button>
-                  <p>3</p>
-                  <button>-</button>
-                </div>
-              </div>
-            </div>
-
-            <hr />
-
-            <div className='cart-product'>
-              <div className='cart-product-detail'>
-                <img src='https://images.urbndata.com/is/image/Anthropologie/40800781_006_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720' />
-                <div>
-                  <p>
-                    <b>Product:</b> Peachy Table
-                  </p>
-                  <p>
-                    <b>Id:</b> 4
-                  </p>
-                  <div
-                    className='cart-product-color'
-                    style={{ backgroundColor: "lightgray" }}
-                  ></div>
-                </div>
-              </div>
-              <div className='cart-product-price'>
-                <div className='cart-product-cost'>
-                  <p>$200</p>
-                </div>
-                <div className='cart-product-amount'>
-                  <button>+</button>
-                  <p>1</p>
-                  <button>-</button>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
 
           <div className='cart-bottom-summary'>
@@ -122,7 +93,9 @@ const Cart = (props) => {
       </div>
       <Footer></Footer>
     </div>
-  );
+  ) : (
+    <h3>Your cart is empty</h3>
+  )
 };
 
 export default Cart;
