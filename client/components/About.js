ccbfdf867e8c379d3ebb38032fde3d8f3845cf31
David Li <dvdli2323@gmail.com>
2021-11-17 17:07:48 -0500

    added team pictures

--- client/components/About.js
+++ client/components/About.js
@@ -10,19 +10,19 @@ const About = () => {
         </div>
         <div className="member-section">
           <div className="member-info">
-            <img src="https://purewows3.imgix.net/images/articles/2019_10/furniture_stores_nyc_interior_define.jpg?auto=format,compress&cs=strip" />
+            <img src="https://ca.slack-edge.com/T024FPYBQ-U02EF7BFCLF-2a8d62d0dd1f-512" />
             <p>David Li</p>
           </div>
           <div className="member-info">
-            <img src="https://purewows3.imgix.net/images/articles/2019_10/furniture_stores_nyc_interior_define.jpg?auto=format,compress&cs=strip" />
+            <img src="https://ca.slack-edge.com/T024FPYBQ-U02EP6J73JA-aa6fd70b38e8-512" />
             <p>Hector Nevarez Gomez</p>
           </div>
           <div className="member-info">
-            <img src="https://purewows3.imgix.net/images/articles/2019_10/furniture_stores_nyc_interior_define.jpg?auto=format,compress&cs=strip" />
+            <img src="Kelsey.jpg" />
             <p>Kelsey Smith</p>
           </div>
           <div className="member-info">
-            <img src="https://purewows3.imgix.net/images/articles/2019_10/furniture_stores_nyc_interior_define.jpg?auto=format,compress&cs=strip" />
+            <img src="Sala.png" />
             <p>Sala Yoshida</p>
           </div>
         </div>
